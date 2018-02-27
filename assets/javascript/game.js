8316682781653a3cf4eb1247783c26e5b6677df1
Max <u1110071@utah.edu>
2018-02-27 16:32:48 -0700

    2.27.3

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,43 +1,118 @@
-/* The setGame Function should span a box on the left with your chosen character, a box in the middle as the "Battleground" and a box/button on the right that contains the available enemies.*/
+var charHealth;
+var charAttk;
+var baseAttk;
+var suchBig;
+var soBrave;
+var enemyHealth;
+var enemyAttk;
+var victory = 0;
 
-function setGame(x) {
-    var element = document.getElementById("chooseChamp1");
-    element.outerHTML = "";
-    delete element;
-    var element = document.getElementById("chooseChamp2");
-}  
-function setChampPic(x) {
-    if (x === 1) {
-      $("#yourChamp").html('<div class="pt-3 inline bg-dark cleric"><img src="assets/images/cleric.png" alt="Human Cleric" class="mb-2" style="height:200px;"><br><h5 class="p-1 text-light border border-light rounded">Pablo the Priest</h5></div>');
-    
-    } else if (x === 2) {
-      $("#yourChamp").html('<div class="pt-3 inline bg-dark mage"><img src="assets/images/girl_mage.png" alt="Human Mage" class="mb-2" style="height:200px;"><br><h5 class="p-1 offset-1 text-info border border-info rounded">Maddie the Mage</h5></div>');
-      
-    } else if (x === 3) {
-      $("#yourChamp").html('<div class="pt-3 inline bg-dark rifleman"><img src="assets/images/rifleman.png" alt="Dwarven Rifleman" class="mb-2" style="height:200px;"><br><h5 class="p-1 text-warning border border-warning rounded">Randy the Rifleman</h5></div>');
-      
-    } else if (x === 4) {
-       $("#yourChamp").html('<div  class="pt-3 inline hamiBox bg-dark"> <img src="assets/images/hamilton.gif" alt="Hamilton the Destroyer" class="hami"><br><h5 class="p-1 text-danger border border-danger rounded">Hamilton the Harasser</h5></div>');
-      
-    } 
+function chooseChamp(x) {
+
+  if (x === 1) {
+    $("#priest2").fadeIn();
+    charHealth = 150;
+    charAttk = 10;
+    baseAttk = 10;
+
+  } else if (x === 2) {
+    $("#mage2").fadeIn();
+    charHealth = 120;
+    charAttk = 20;
+    baseAttk = 20;
+
+  } else if (x === 3) {
+    $("#rifleman2").fadeIn();
+    charHealth = 180;
+    charAttk = 12;
+    baseAttk = 12;
+
+  } else if (x === 4) {
+    $("#hamiBox2").fadeIn();
+    charHealth = 80;
+    charAttk = 3;
+    baseAttk = 3;
+    soBrave = true;
+
+  } 
 }
 
 function chooseFoe(x) {
   $("#battleground").show();
+  $("#chooseEnemy1").hide();
+  $("#chooseEnemy2").show();
 
   if (x === 1) {
     $("#goblin1").fadeIn();
     $("#goblin2").fadeOut();
+    enemyHealth = 80;
+    $(".enemyBar").html('Health: ' + enemyHealth);
+    enemyAttk = 5;
+    suchBig = false;
+
   } else if (x === 2) {
     $("#orc1").fadeIn();
     $("#orc2").fadeOut();
+    enemyHealth = 150;
+    $(".enemyBar").html('Health: ' + enemyHealth);
+    enemyAttk = 10;
+    suchBig = false;
+
   } else if (x === 3) {
     $("#fire1").fadeIn();
     $("#fire2").fadeOut();
+    enemyHealth = 200;
+    $(".enemyBar").html('Health: ' + enemyHealth);
+    enemyAttk = 20;
+    suchBig = false;
+
   } else if (x === 4) {
     $("#elemental1").fadeIn();
     $("#elemental2").fadeOut();
+    enemyHealth = 300;
+    $(".enemyBar").html('Health: ' + enemyHealth);
+    enemyAttk = 25;
+    suchBig = true;
+
   } 
+}
+
+function attackEnemy() {
+
+  enemyHealth = enemyHealth - charAttk;
+  $(".enemyBar").html('Health: ' + enemyHealth);
+  
+  if (enemyHealth <= 0) {
+    $("#goblin1").fadeOut();
+    $("#orc1").fadeOut();
+    $("#fire1").fadeOut();
+    $("#elemental1").fadeOut();
+    victory++;
+
+    if (victory === 4) {
+      $("#fightScreen").fadeOut(500, function() { $("#gameWon").fadeIn(); })
+    }
+
+    return;
+  }
+
+  if (suchBig === true && soBrave === true) {
+    charHealth += 10;
+  } else {
+    charHealth = charHealth - enemyAttk;
+  }
+  $(".healthBar").html("Health: " + charHealth);
+
+  upgradeAttk();
+  $(".attackBar").html("Attack: " + charAttk);
+
+  if (charHealth <= 0) {
+    $("#fightScreen").fadeOut(500, function() { $("#gameLost").fadeIn(); })
+  }
+}
+
+function upgradeAttk() {
+    charAttk = charAttk + baseAttk;
 }
 
 $("#fightScreen").hide();
@@ -46,9 +121,16 @@ $("#goblin1").hide();
 $("#orc1").hide();
 $("#fire1").hide();
 $("#elemental1").hide();
+$("#priest2").hide();
+$("#mage2").hide();
+$("#rifleman2").hide();
+$("#hamiBox2").hide();
+$("#chooseEnemy2").hide();
+$("#gameLost").hide();
+$("#gameWon").hide();
 
 
-$("#resetMe").click(function() {
+$(".resetMe").click(function() {
   location.reload();
 });
 
